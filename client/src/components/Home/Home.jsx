aaa50166ed39905f79cc4c32103f9a7f85e7590a
Frank Wember <wemberfrankd@gmail.com>
2024-07-03 10:11:00 -0700

    Update Home.jsx

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,16 +4,16 @@ import SideBar from "../SideBar/SideBar";
 import MediaContainer from "./MediaContainer";
 import RecommendationBar from "../RecommendationBar/RecommendationBar";
 
-const Home = () => {
+const Home = ({ token }) => {
   return (
     <div className="d-flex">
       <SideBar />
-      {/* <div className="main-content">
-        <MediaContainer />
+      <div className="main-content">
+        <MediaContainer token={token} /> {/* Pass the token as a prop */}
       </div>
       <div>
         <RecommendationBar />
-      </div> */}
+      </div>
     </div>
   );
 };
