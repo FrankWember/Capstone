a7e8a62caef185ae05f347b6c558ebc5cca7a4a9
Frank Wember <127029813+FrankWember@users.noreply.github.com>
2024-07-18 16:55:33 -0700

    I created help functions to make the implementation of the recommendation function more modular

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -108,7 +108,7 @@ const App = () => {
         }
       }
     };
-
+    refreshAccessToken();
     const interval = setInterval(refreshAccessToken, 55 * 60 * 1000);
     return () => clearInterval(interval);
   }, []);
